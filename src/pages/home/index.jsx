12683a522f9905387cb484de9cff78cec767457a
Justin Martinez <justinmartinez14@gmail.com>
2021-02-22 23:06:18 -0700

    remove default files and add home page

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import style from "./home.module.scss";
+
+import React from "react";
+
+const Home = () => (
+  <div className={style.container}>
+    <div className={style.title}>Restaurant Finder 2021</div>
+  </div>
+);
+
+export default Home;
